6c428b67251bb950745c1cd0c9934b962d66d3d8
Charlotte Fereday <cfereday@thoughtworks.com>
2018-03-11 21:38:41 +0000

    [Charlotte] Adds in spike for abstracting keys from an object & adding to an array

---
+++ spike/spike.js
@@ -0,0 +1,19 @@
+export const reasonAndEffectiveDateToArray = (object) => {
+    let value1 = object['reason'];
+    let value2 = object['effectiveDate'];
+    return [['reason', value1], ['effectiveDate', value2]];
+};
+
+export const transitionsToArray = (object) => {
+    let transitions = object['transitions'];
+    let keys = Object.keys(transitions);
+    let values = Object.values(transitions);
+
+    return keys.map((element, index) => {
+        return [element, values[index]];
+    });
+};
+
+export const concatArrays = (array1, array2) => {
+    return array1.concat(array2);
+};
---
+++ spike/spike.test.js
@@ -0,0 +1,27 @@
+import { reasonAndEffectiveDateToArray, transitionsToArray, concatArrays } from './spike';
+
+describe('keyAndValueToArray', () => {
+    const history = {
+        'transitions': { 'type': 'APPLY',
+            'from': '21-03-18',
+            'to': '21-03-19'},
+        'effectiveDate': '04-09-19',
+        'reason': 'GENERAL',
+    };
+
+    let reasonAndEffectiveDate =[['reason', 'GENERAL'], ['effectiveDate', '04-09-19']];
+    let transitions = [['type', 'APPLY'], ['from', '21-03-18'], ['to', '21-03-19']];
+    let concatonated = [['reason', 'GENERAL'], ['effectiveDate', '04-09-19'], ['type', 'APPLY'], ['from', '21-03-18'], ['to', '21-03-19']];
+
+    test('puts the key & value pair of reason & effectiveDate into a nested array', () => {
+        expect(reasonAndEffectiveDateToArray(history)).toEqual([['reason', 'GENERAL'], ['effectiveDate', '04-09-19']]);
+    });
+
+    test('puts the key & value pair of transitions value object into a nested array', () => {
+        expect(transitionsToArray(history)).toEqual([['type', 'APPLY'], ['from', '21-03-18'], ['to', '21-03-19']]);
+    });
+
+    test('joins reasonAndEffectiveDate and transitions into a single array', () => {
+        expect(concatArrays(reasonAndEffectiveDate, transitions)).toEqual(concatonated);
+    });
+});
